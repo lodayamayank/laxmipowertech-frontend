996f5af7f28ac53891fb616bf29a075f86860d4d
Mayank Lodaya <lodaya.mayank@gmail.com>
2025-08-04 23:54:16 +1000

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // 👈 proxies /api to backend
+      '/api': 'https://laxmipowertech-backend.onrender.com', // 👈 proxies /api to backend
     },
   },
 });
